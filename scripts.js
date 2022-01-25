28e58b936162a11c03bc25c3631d3ed03ed7a84e
PJ Jones <pj.jones4000.com>
2022-01-25 13:04:56 -0600

    got a button that executes the console log version of function on click

--- scripts.js
+++ scripts.js
@@ -1,34 +1,48 @@
-let friends = ["Alyssa", "Larry", "Amanda", "Thomas", "Phillip"];
+document.addEventListener("DOMContentLoaded", function () {
+  let btnDiv = document.createElement("div");
 
-let makeSong = () => {
-  for (let j = 0; j < friends.length; j++) {
-    for (let i = 99; i > 0; i--) {
-      if (i === 2) {
-        console.log(
-          `${i} lines of code in the file, ${i} lines of code; ${
-            friends[j]
-          } strikes one out, clears it all out, ${
-            i - 1
-          } line of code in the file`
-        );
-      } else if (i === 1) {
-        console.log(
-          `${i} line of code in the file, ${i} line of code; ${
-            friends[j]
-          } strikes one out, clears it all out, ${
-            i - 1
-          } lines of code in the file`
-        );
-      } else {
-        console.log(
-          `${i} lines of code in the file, ${i} lines of code; ${
-            friends[j]
-          } strikes one out, clears it all out, ${
-            i - 1
-          } lines of code in the file`
-        );
+  document.body.appendChild(btnDiv);
+
+  let button = document.createElement("button");
+  let btnText = document.createTextNode("Sing!");
+  button.className = "button";
+  button.appendChild(btnText);
+
+  btnDiv.appendChild(button);
+
+  let makeSong = () => {
+    for (let j = 0; j < friends.length; j++) {
+      for (let i = 99; i > 0; i--) {
+        if (i === 2) {
+          console.log(
+            `${i} lines of code in the file, ${i} lines of code; ${
+              friends[j]
+            } strikes one out, clears it all out, ${
+              i - 1
+            } line of code in the file`
+          );
+        } else if (i === 1) {
+          console.log(
+            `${i} line of code in the file, ${i} line of code; ${
+              friends[j]
+            } strikes one out, clears it all out, ${
+              i - 1
+            } lines of code in the file`
+          );
+        } else {
+          console.log(
+            `${i} lines of code in the file, ${i} lines of code; ${
+              friends[j]
+            } strikes one out, clears it all out, ${
+              i - 1
+            } lines of code in the file`
+          );
+        }
       }
     }
-  }
-};
-makeSong();
+  };
+
+  button.addEventListener("click", makeSong);
+
+  let friends = ["Alyssa", "Larry", "Amanda", "Thomas", "Phillip"];
+});
