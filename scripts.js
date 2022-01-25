d519784619231d8eed1e1ad98e1905f543e3c076
PJ Jones <pj.jones4000.com>
2022-01-25 13:26:56 -0600

    it does everything it's supposed to do, not sure if the h1 is still supposed to be in the html but leaving it for now, everything else is DOM

--- scripts.js
+++ scripts.js
@@ -10,33 +10,51 @@ document.addEventListener("DOMContentLoaded", function () {
 
   btnDiv.appendChild(button);
 
+  let friends = ["Alyssa", "Larry", "Amanda", "Thomas", "Phillip"];
+
   let makeSong = () => {
     for (let j = 0; j < friends.length; j++) {
+      let friendDiv = document.createElement('div');
+      friendDiv.className = "friend";
+      let friendH3 = document.createElement('h3');
+      let h3Text = document.createTextNode(friends[j]);
+      friendH3.appendChild(h3Text);
+      friendDiv.appendChild(friendH3);
+      document.body.appendChild(friendDiv);
       for (let i = 99; i > 0; i--) {
         if (i === 2) {
-          console.log(
+          let friendP = document.createElement('p');
+          let friendPText = document.createTextNode(
             `${i} lines of code in the file, ${i} lines of code; ${
               friends[j]
             } strikes one out, clears it all out, ${
               i - 1
             } line of code in the file`
           );
+          friendP.appendChild(friendPText);
+          friendDiv.appendChild(friendP);
         } else if (i === 1) {
-          console.log(
+          let friendP = document.createElement('p');
+          let friendPText = document.createTextNode(
             `${i} line of code in the file, ${i} line of code; ${
               friends[j]
             } strikes one out, clears it all out, ${
               i - 1
             } lines of code in the file`
           );
+          friendP.appendChild(friendPText);
+          friendDiv.appendChild(friendP);
         } else {
-          console.log(
+          let friendP = document.createElement('p');
+          let friendPText = document.createTextNode(
             `${i} lines of code in the file, ${i} lines of code; ${
               friends[j]
             } strikes one out, clears it all out, ${
               i - 1
             } lines of code in the file`
           );
+          friendP.appendChild(friendPText);
+          friendDiv.appendChild(friendP);
         }
       }
     }
@@ -44,5 +62,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
   button.addEventListener("click", makeSong);
 
-  let friends = ["Alyssa", "Larry", "Amanda", "Thomas", "Phillip"];
+  
 });
